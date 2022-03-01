54fd72b1e38fe653cab142fe870fbe86fe87615e
fikryakrom <mohammad.fikry@ui.ac.id>
2022-03-01 10:20:34 +0700

    reviewing and fixing code

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import ScoreboardView from "./scoreboard/ScoreboardView.js";
+import ScoreboardView from "./scoreboard/ScoreboardView.js"
 
 let playerOneScore = 0;
 let playerTwoScore = 0;
 const root = document.querySelector("#app");
-const view = new ScoreboardView(root, "Player One", "Player Two", (player, direction) => {
+const view = new ScoreboardView(root, "Japan", "South Korea", (player, direction) => {
 	const difference = direction === "minus" ? -1 : 1;
-
-	if (player === "one") {
+	
+	if(player === "Japan") {
 		playerOneScore = Math.max(playerOneScore + difference, 0);
 	} else {
 		playerTwoScore = Math.max(playerTwoScore + difference, 0);
@@ -14,4 +14,3 @@ const view = new ScoreboardView(root, "Player One", "Player Two", (player, direc
 
 	view.update(playerOneScore, playerTwoScore);
 });
-
--- scoreboard/ScoreboardView.js
+++ scoreboard/ScoreboardView.js
@@ -1,35 +1,32 @@
 export default class ScoreboardView {
-	constructor(root, playerOneName, playerTwoName, onControlButtonClick) {
+	constructor(root, Japan, SouthKorea, onControlButtonClick) {
 		this.root = root;
 		this.root.innerHTML = `
-			<div class="scoreboard">
-				<div class="scoreboard__name scoreboard__name--one">${playerOneName}</div>
-				<div class="scoreboard__name scoreboard__name--two">${playerTwoName}</div>
-				<div class="scoreboard__score" data-for-player="one">0</div>
-				<div class="scoreboard__score" data-for-player="two">0</div>
-				<div class="scoreboard__controls" data-for-player="one">
-					<button class="scoreboard__control-button">-</button>
-					<button class="scoreboard__control-button">+</button>
-				</div>
-				<div class="scoreboard__controls" data-for-player="two">
-					<button class="scoreboard__control-button">-</button>
-					<button class="scoreboard__control-button">+</button>
-				</div>
+		<div class="scoreboard">
+			<div class="scoreboard__name scoreboard__name--one">${"Japan"}</div>
+			<div class="scoreboard__name scoreboard__name--two">${"South Korea"}</div>
+			<div class="scoreboard__score" data-for-player="Japan">0</div>
+			<div class="scoreboard__score" data-for-player="South Korea">0</div>
+			<div class="scoreboard__controls" data-for-player="Japan">
+				<button class="scoreboard__control-button">-</button>
+				<button class="scoreboard__control-button">+</button>
 			</div>
-		`;
-
-		this.root.querySelectorAll(".scoreboard__control-button").forEach(controlButton => {
-			controlButton.addEventListener("click", () => {
-				const direction = controlButton.textContent === "-" ? "minus" : "plus";
-				const player = controlButton.closest(".scoreboard__controls").dataset.forPlayer;
-
-				onControlButtonClick(player, direction);
-			});
+			<div class="scoreboard__controls" data-for-player="South Korea">
+				<button class="scoreboard__control-button">-</button>
+				<button class="scoreboard__control-button">+</button>
+			</div>
+		</div>
+	`;
+	this.root.querySelectorAll(".scoreboard__control-button").forEach(controlButton => {
+		controlButton.addEventListener("click", () => {
+			const direction = controlButton.textContent === "-" ? "minus" : "plus";
+			const player = controlButton.closest(".scoreboard__controls").dataset.forPlayer;
+			onControlButtonClick(player, direction);
 		});
+	});
 	}
-
 	update(playerOneScore, playerTwoScore) {
-		this.root.querySelector(".scoreboard__score[data-for-player='one']").textContent = playerOneScore;
-		this.root.querySelector(".scoreboard__score[data-for-player='two']").textContent = playerTwoScore;
+		this.root.querySelector(".scoreboard__score[data-for-player='Japan']").textContent = playerOneScore;
+		this.root.querySelector(".scoreboard__score[data-for-player='South Korea']").textContent = playerTwoScore;
 	}
 }
